2ecb33df2c5aa4e0c2928f0db1cf9dd75bc33331
pigu4637 <pigu4637@colorado.edu>
2019-03-19 10:19:01 -0600

    Update refdata_monitor.py

--- GUI/refdata_monitor.py
+++ GUI/refdata_monitor.py
@@ -27,8 +27,11 @@ import os as os
 # pySerial
 import serial
 
-#Threading
+# Threading
 import threading
+
+# Sys
+import sys
 
 # Matplotlib
 import matplotlib
@@ -53,9 +56,24 @@ data_template =['  1','  2','  3','  4','  5','  6','  7','  8','  9',' 10',' 11
                 ' 17',' 18',' 19',' 20',' 21',' 22',' 23',' 24',' 25',' 26',' 27',' 28',' 29',' 30',' 31',' 32',
                 ' 33',' 34',' 35',' 36']
 b1_rec = False
-heat1 = 0
-heat2 = 0
+b2_rec = False
+b3_rec = False
+heat1 = [0, 0, 0, 0, 0, 0]
+heat2 = [0, 0, 0, 0, 0, 0]
 errorlist = []
+
+errorlist.append(dt.datetime.now())
+errorlist.append(matplotlib.dates.date2num(dt.datetime.now()))
+
+# -- Serial --
+# Serial ports
+
+serial1 = serial.Serial(timeout=1,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS)
+serial1.baudrate = 9600
+serial1.port = 'COM3'
+errorlist.append('Port ' + serial1.name + ' used.')
+errorlist.append('---')
+
 
 # -- Threads ---
 
@@ -128,7 +146,9 @@ def seriallisten():
                             #print(serial_data1[i])
 
             except Exception as e:
+                errorlist.append('{}'.format(sys.exc_info()[-1].tb_lineno))
                 errorlist.append(e)
+                errorlist.append('---')
 
 # --- Classes ---
 
@@ -172,11 +192,14 @@ class MonitorFrame():
         self.controls.columnconfigure(2, weight=1)
         self.controls.columnconfigure(3, weight=1)
         self.controls.rowconfigure(0, weight=1)
-        
+
+        self.save_status = 0
+        self.save_index = 0
+        self.save_file_exists = False
         self.commentbox = tk.Text(self.controls)
         self.commentbox.place(height=195,width=675,x=750,y=10)
-        self.saveone = ttk.Button(self.controls, text = 'Save Once', style='button.TButton',state=tk.DISABLED)
-        self.savemultiple = ttk.Button(self.controls, text = 'Save Continuously', style='button.TButton',state=tk.DISABLED)
+        self.saveone = ttk.Button(self.controls, text = 'Save Once', style='button.TButton',state=tk.DISABLED, command=lambda : self.save_data(1))
+        self.savemultiple = ttk.Button(self.controls, text = 'Save Continuously', style='button.TButton',state=tk.DISABLED, command=lambda : self.save_data(2))
         self.savingcont = ttk.Frame(self.controls, style = 'off.TFrame')
         self.saveone.place(height=58,width=133,x=750,y=217)
         self.savemultiple.place(height=58,width=133,x=895,y=217)
@@ -187,11 +210,55 @@ class MonitorFrame():
         self.rd = ReceiveData()
         self.creategraphs()
 
+
+    def save_data(self,ind):
+        try:
+            if self.save_file_exists == False:
+                self.filename = 'refdata' + str(round(matplotlib.dates.date2num(dt.datetime.now())*100000)) + '.txt'
+                self.filename = os.path.join(self.saveloc, self.filename)
+                print(self.filename)
+                self.open_savefile = open(self.filename,"w+")
+                self.save_file_exists = True
+            else:
+                self.open_savefile = open(self.filename,"a")
+            if ind == 1:
+                self.savingcont = ttk.Frame(self.controls, style = 'error.TFrame')
+
+                to_write = str(self.rd.rec_data1_2[-1])
+                for i in range(36):
+                    to_write = to_write + ' ' + str(self.rd.rec_data1_1[i][-1])
+                to_write = to_write + '\n'
+                self.open_savefile.write(to_write)
+                self.savingcont = ttk.Frame(self.controls, style = 'off.TFrame')
+            if ind == 2:
+                if self.save_status == 0:
+                    self.save_index = self.rd.rec_data1_2[-1]
+                    self.save_status = 1
+                    self.savingcont = ttk.Frame(self.controls, style = 'on.TFrame')
+                else:
+                    self.save_status = 0
+                    self.savingcont = ttk.Frame(self.controls, style = 'error.TFrame')
+                    for j in range(self.save_index, self.rd.rec_data1_2[-1]):
+                        to_write = str(self.rd.rec_data1_2[j])
+                        for i in range(36):
+                            to_write = to_write + ' ' + str(self.rd.rec_data1_1[i][j])
+                        to_write = to_write + '\n'
+                        self.open_savefile.write(to_write)
+                    self.savingcont = ttk.Frame(self.controls, style = 'off.TFrame')
+
+            self.open_savefile.close()
+        except Exception as e:
+            errorlist.append('{}'.format(sys.exc_info()[-1].tb_lineno))
+            errorlist.append(e)
+            errorlist.append('---')
+
     def initialize_table(self):
         try:
             self.datatable.destroy()
         except Exception as e:
+            errorlist.append('{}'.format(sys.exc_info()[-1].tb_lineno))
             errorlist.append(e)
+            errorlist.append('---')
         self.datatable = ttk.Frame(self.mainframe,style = 'table.TFrame')
         self.datatable.place(height=125, width=1450, x=25, y=420)
         self.databox = []
@@ -264,7 +331,7 @@ class MonitorFrame():
         except:
             self.logwindow = tk.Toplevel(root)
             self.logwindow.title('Configuration')
-            self.logwindow.geometry("500x690")
+            self.logwindow.geometry("500x725")
             self.logwindow.resizable(width=False, height=False)
             self.logframe = LogFrame(self.logwindow)
 
@@ -281,7 +348,9 @@ class MonitorFrame():
                     self.state1 = 1
                 except Exception as e:
                     self.data1_status.configure(style='error.TFrame')
+                    errorlist.append('{}'.format(sys.exc_info()[-1].tb_lineno))
                     errorlist.append(e)
+                    errorlist.append('---')
         elif ind == 2:
             if self.state2 == 1:
                 self.data2_status.configure(style='off.TFrame')
@@ -294,7 +363,7 @@ class MonitorFrame():
         self.saveloc = askdirectory()
         if self.saveloc == '':
             self.saveloc= os.getcwd()
-            print(os.getcwd())
+        print(self.saveloc)
         self.saveone.configure(state=tk.NORMAL)
         self.savemultiple.configure(state=tk.NORMAL)
 
@@ -306,13 +375,21 @@ class MonitorFrame():
 
         self.canvas1 = FigureCanvasTkAgg(self.fig1, self.graph1)
         self.canvas1.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand = True)
-        self.fig1_info.plot([0],[0])
+        self.fig1_info.plot([0],[0], 'fuchsia', label='Legend')
+        self.legend1 = self.fig1_info.legend(loc='upper center', shadow=False, fontsize='large')
+        self.legend1.get_frame().set_facecolor('white')
         self.canvas1.draw()
 
-        self.fig2_info.plot(0,0)
+        self.fig2_info.plot(0,0, 'fuchsia', label='Legend')
+        self.legend2 = self.fig2_info.legend(loc='upper center', shadow=False, fontsize='large')
+        self.legend2.get_frame().set_facecolor('white')
         self.canvas2 = FigureCanvasTkAgg(self.fig2, self.graph2)
         self.canvas2.draw()
         self.canvas2.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand = True)
+
+        self.graph_colors = ['aqua', 'azure', 'blue', 'cyan', 'chartreuse', 'coral', 'brown', 'crimson', 'darkblue', 'darkgreen', 'fuchsia', 'gold', 'grey', 'khaki',
+                             'green', 'lightblue', 'lavender', 'lightgreen', 'magenta', 'orange', 'pink', 'purple', 'red', 'sienna', 'silver', 'teal', 'violet','yellow',
+                             'yellowgreen', 'white', 'tan', 'salmon', 'navy', 'ivory', 'beige', 'black']
 
     def updategraphs(self,fignum,datapoints):
         if fignum == 1:
@@ -320,18 +397,22 @@ class MonitorFrame():
             lastind = len(self.rd.rec_data1_1[1][:])
             for i in datapoints:
                 if lastind > 10:
-                    self.fig1_info.plot(self.rd.rec_data1_2[lastind-10:lastind].tolist(),self.rd.rec_data1_1[i][lastind-10:lastind].tolist())
+                    self.fig1_info.plot(self.rd.rec_data1_2[lastind-10:lastind].tolist(),self.rd.rec_data1_1[i][lastind-10:lastind].tolist(), self.graph_colors[i-1], label=('Grid-'+str(i)))
                 else:
-                    self.fig1_info.plot(self.rd.rec_data1_2[0:lastind],self.rd.rec_data1_1[i][0:lastind])
+                    self.fig1_info.plot(self.rd.rec_data1_2[0:lastind],self.rd.rec_data1_1[i][0:lastind], self.graph_colors[i-1], label=('Grid-'+str(i)))
+                self.legend1 = self.fig1_info.legend(loc='upper center', shadow=False, fontsize='large')
+                self.legend1.get_frame().set_facecolor('C0')
                 self.canvas1.draw()
         elif fignum == 2:
             self.fig2.clf
             lastind = len(self.rd.rec_data2_1[:][1])
-            for i in range(36):
+            for i in datapoints:
                 if lastind > 10:
-                    self.fig2_info.plot(self.rd.rec_data2_1[i][lastind-10:lastind],self.rd.rec_data2_2[lastind-10:lastind])
+                    self.fig2_info.plot(self.rd.rec_data2_1[i][lastind-10:lastind],self.rd.rec_data2_2[lastind-10:lastind], self.graph_colors[i-1], label=('Grid-'+str(i)))
                 else:
-                    self.fig2_info.plot(self.rd.rec_data2_1[i][0:lastind],self.rd.rec_data2_2[0:lastind])
+                    self.fig2_info.plot(self.rd.rec_data2_1[i][0:lastind],self.rd.rec_data2_2[0:lastind], self.graph_colors[i-1], label=('Grid-'+str(i)))
+                self.legend2 = self.fig2_info.legend(loc='upper center', shadow=False, fontsize='large')
+                self.legend2.get_frame().set_facecolor('C0')
                 self.canvas2.draw()
             
 class TestFrame():
@@ -351,7 +432,9 @@ class TestFrame():
                 else:
                     return 0
             except Exception as e:
+                errorlist.append('{}'.format(sys.exc_info()[-1].tb_lineno))
                 errorlist.append(e)
+                errorlist.append('---')
                 return -1
         else:
             return 0
@@ -367,23 +450,83 @@ class TestFrame():
                 else:
                     return 0
             except Exception as e:
+                errorlist.append('{}'.format(sys.exc_info()[-1].tb_lineno))
                 errorlist.append(e)
+                errorlist.append('---')
                 return -1
         else:
             return 0
         
     def write_test(self,ind):
-        return 0
+        if ind == 1:
+            try:
+                if serial1.isOpen():
+                    if b2_rec == True:
+                        return 1
+                    else:
+                        return 0
+                else:
+                    return 0
+            except Exception as e:
+                errorlist.append('{}'.format(sys.exc_info()[-1].tb_lineno))
+                errorlist.append(e)
+                ferrorlist.append('---')
+                return -1
+        else:
+            return 0
+        
     def read_test(self,ind):
-        return 0
+        if ind == 1:
+            try:
+                if serial1.isOpen():
+                    if b3_rec == True:
+                        return 1
+                    else:
+                        return 0
+                else:
+                    return 0
+            except Exception as e:
+                errorlist.append('{}'.format(sys.exc_info()[-1].tb_lineno))
+                errorlist.append(e)
+                errorlist.append('---')
+                return -1
+        else:
+            return 0
+        
     def temp_test(self,ind):
-        return 0
+        try:
+            partial = 0
+            all_temp = 1
+            if ind == 1:
+                if sum(serial_data1) == 0:
+                    all_temp = 0
+                else:
+                    for i in serial_data1[0:36]:
+                        if i != 0:
+                            partial = 1
+                        else:
+                            all_temp = 0
+            if all_temp == 1:
+                return 1
+            elif partial == 1:
+                return 2
+            else:
+                return 0
+        except Exception as e:
+            errorlist.append('{}'.format(sys.exc_info()[-1].tb_lineno))
+            errorlist.append(e)
+            errorlist.append('---')
+            return -1
+        
+                
 
     def runtest(self,ind):
         try:
             self.mainframe.destroy()
         except Exception:
+            errorlist.append('{}'.format(sys.exc_info()[-1].tb_lineno))
             errorlist.append(e)
+            errorlist.append('---')
         self.mainframe = ttk.Frame(self.par, style = 'second.TFrame' )
         self.mainframe.place(height=310, width=280, x=10, y=10)
         for i in range(1,11):
@@ -432,6 +575,8 @@ class TestFrame():
             tk.Label(self.mainframe,text='GOOD CONNECTION',font=('fixedsys',12),background='green3').grid(column=0,row=10)
         elif res == 0:
             tk.Label(self.mainframe,text='NO CONNECTION',font=('fixedsys',12),background='tomato').grid(column=0,row=10)
+        elif res == 2:
+            tk.Label(self.mainframe,text='PARTIAL SUCCESS',font=('fixedsys',12),background='orange').grid(column=0,row=10)
         else:
             tk.Label(self.mainframe,text='ERROR - CHECK LOGS',font=('fixedsys',12),background='lightgrey').grid(column=0,row=10)
         self.refreshbutton.configure(state = 'normal')
@@ -439,25 +584,41 @@ class TestFrame():
 class LogFrame():
     def __init__(self,parent):
         self.mainframe = ttk.Frame(parent, style = 'second.TFrame' )
-        self.mainframe.place(height=670, width=480, x=10, y=10)
+        self.mainframe.place(height=705, width=480, x=10, y=10)
         self.errorbox = ScrolledText(self.mainframe,state=tk.DISABLED)
-        self.errorbox.place(height=274, width=470,x=5,y=386)
+        self.errorbox.place(height=274, width=470,x=5,y=420)
         self.errorbox.configure(state='normal')
         for i in errorlist:
             self.errorbox.insert(tk.END,str(i))
             self.errorbox.insert(tk.END,'\n\n')
         self.errorbox.configure(state=tk.DISABLED)
 
-        self.heater1_slider = tk.Scale(self.mainframe, from_=0, to=100,orient=tk.HORIZONTAL)
-        self.heater1_slider.place(height= 58, width=240, x=10,y=250)
-        self.heater1_slider.set(heat1)
-        self.confirm1 = ttk.Button(self.mainframe, text ="Confirm (Heater 1)",style='button.TButton', command=lambda : self.heaterconfirm(1))
-        self.confirm1.place(height = 58, width = 200, x = 270, y = 250)
-        self.heater2_slider = tk.Scale(self.mainframe, from_=0, to=100,orient=tk.HORIZONTAL)
-        self.heater2_slider.place(height= 58, width=240, x=10,y=318)
-        self.heater2_slider.set(heat2)
-        self.confirm2 = ttk.Button(self.mainframe, text ="Confirm (Heater 2)",style='button.TButton', command=lambda : self.heaterconfirm(2))
-        self.confirm2.place(height = 58, width = 200, x = 270, y = 318)
+        self.heater_nb = ttk.Notebook(self.mainframe)
+        self.heater_nb.place(x=5, y=250, height=160, width=470)
+        self.heater_frame = [0, 0, 0, 0 ,0 ,0]
+        self.heater1_slider = [0, 0, 0, 0, 0 ,0]
+        self.heater2_slider = [0, 0, 0, 0, 0 ,0]
+        self.confirm1 = [0, 0, 0, 0, 0 ,0]
+        self.confirm2 = [0, 0, 0, 0, 0 ,0]
+
+        for i in range(6):
+            self.heater_frame[i] = ttk.Frame(self.heater_nb, style = 'second.TFrame')
+            self.heater_nb.add(self.heater_frame[i], text='Tray '+str(i+1))
+
+        for i in range(6):     
+            self.heater1_slider[i] = tk.Scale(self.heater_frame[i], from_=0, to=100,orient=tk.HORIZONTAL)
+            self.heater1_slider[i].place(height= 58, width=240, x=5,y=5)
+            self.heater1_slider[i].set(heat1[i])
+            self.confirm1[i] = ttk.Button(self.heater_frame[i], text ="Confirm (Heater 1)",style='button.TButton', command=lambda : self.heaterconfirm(1))
+            self.confirm1[i].place(height = 58, width = 200, x = 260, y = 5)
+            self.heater2_slider[i] = tk.Scale(self.heater_frame[i], from_=0, to=100,orient=tk.HORIZONTAL)
+            self.heater2_slider[i].place(height= 58, width=240, x=5,y=68)
+            self.heater2_slider[i].set(heat2[i])
+            self.confirm2[i] = ttk.Button(self.heater_frame[i], text ="Confirm (Heater 2)",style='button.TButton', command=lambda : self.heaterconfirm(2))
+            self.confirm2[i].place(height = 58, width = 200, x = 260, y = 68)
+
+        self.heater_nb.select(self.heater_frame[0])
+        self.heater_nb.enable_traversal()
 
         self.grid_list = tk.StringVar()
         self.grid_list.set("Grid-01 Grid-02 Grid-03 Grid-04 Grid-05 Grid-06 Grid-07 Grid-08 \
@@ -492,8 +653,8 @@ class ReceiveData():
     def __init__(self):
         self.rec_data1_1 = np.array([[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]], np.int32)
         self.rec_data2_1 = np.array([[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]], np.int32)
-        self.rec_data1_2 = np.array([], np.int32)
-        self.rec_data2_2 = np.array([], np.int32)
+        self.rec_data1_2 = np.array([[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]], np.int32)
+        self.rec_data2_2 = np.array([[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]], np.int32)
         self.time_ind = 0
         self.datapoints = [1,2,3,4,5,6]
         
@@ -528,17 +689,8 @@ s.configure('error.TFrame',background='orange',relief='raised')
 # Frame creations
 mf = MonitorFrame(root)
 
-# Serial ports
-
-serial1 = serial.Serial(timeout=1,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS)
-serial1.baudrate = 9600
-serial1.port = 'COM3'
-print('Port ' + serial1.name + ' used.')
-
 
 #------
-print(dt.datetime.now())
-#print(matplotlib.dates.date2num(dt.datetime.now()))
 
 thr1 = threading.Thread(target=seriallisten)
 thr1.start()
