c7059211ab440bdbc4e40b726816ee922c0e44ee
pigu4637 <pigu4637@colorado.edu>
2019-04-11 15:58:54 -0600

    Update ADCcontroller.ino

--- ADC software/ADCcontroller.ino
+++ ADC software/ADCcontroller.ino
@@ -4,7 +4,7 @@ String inputString = "";
 bool stringComplete = false;
 String outputString = "";
 
-const int adc_address[5] = {0x1D, 0x1E, 0x35, 0x2F, 0x2E};
+const int adc_address[5] = {0x1D, 0x2F, 0x35,0x1E, 0x2E};
 const int adc_channel[8] = {0x20,0x21,0x22,0x23,0x24,0x25,0x26,0x27};
 const int adc_limit_high[8] = {0x2A,0x2C,0x2E,0x30,0x32,0x34,0x36,0x38};
 const int adc_limit_low[8] = {0x2B,0x2D,0x2F,0x31,0x33,0x35,0x37,0x39};
@@ -16,14 +16,10 @@ const int conv_rate = B00000001; // Continuous conversion mode
 signed int adc_data[40];
 // 8*n + i | n - 0:4 | i - 0:7 
 
-long int count = 0;
-char ser_output_begd[5] = {'b','e','g','d','\n'};
-char ser_output_3[5] = {' ',' ',' ',' ','\n'};
-String poss_inputs[3] = {"b1\n", "dreq\n", "b2\n"};
-char ser_output_b1c[4] = {'b','1','c','\n'};
-String temp_output;
 char ser_output_b2c[4] = {'b','2','c','\n'};
 char ser_output_b2n[4] = {'b','2','n','\n'};
+long int count = 0;
+String temp_output;
 
 void setup() {
   Wire.begin();
@@ -73,16 +69,14 @@ void setup() {
 
 void loop() {
   if (stringComplete) {
+    String poss_inputs[3] = {"b1\n", "dreq\n", "b2\n"};
     outputString = inputString;
     if(outputString == poss_inputs[0]) {
-      Serial.write(ser_output_b1c);
-      Serial.flush();
-      delay(10);
+      Serial.write("b1c\n");
     }
     else if(outputString == poss_inputs[1]) {
-      Serial.write(ser_output_begd);
-
-      delay(100);
+      char ser_output_3[5] = {' ',' ',' ',' ','\n'};
+      Serial.write("begd\n");
       for (int i = 0; i < 40; i++){
         temp_output = String(adc_data[i]);
         switch(temp_output.length()){
@@ -123,15 +117,13 @@ void loop() {
             break;
         }
         Serial.write(ser_output_3);
-        delay(100);
-        //Serial.write(ser_output_3);
       }
       ser_output_3[0] = 'e';
       ser_output_3[1] = 'n';
       ser_output_3[2] = 'd';
       ser_output_3[3] = 'd';
       ser_output_3[4] = '\n';
-      Serial.print(ser_output_3);
+      Serial.write(ser_output_3);
     }
     else if(outputString == poss_inputs[2]) {
       bool readok = 1;
@@ -152,14 +144,11 @@ void loop() {
           writeok = 0;
         }
       }
-      delay(100);
       if(readok == 1 && writeok == 1) {
-        Serial.print(ser_output_b2c);
-        delay(100);
+        Serial.write(ser_output_b2c);
       }
       else if(readok == 1 && writeok != 1) {
-        Serial.print(ser_output_b2n);
-        delay(100);
+        Serial.write(ser_output_b2n);
       }
     }
     inputString = "";
